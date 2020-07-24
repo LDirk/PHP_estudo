28b05fcd744a5a377f7f6bc18e07c2b12ecd7f44
LDirk <47995676+LDirk@users.noreply.github.com>
2020-07-24 14:20:43 -0300

    Update index.php

--- PHP - POO/Criando_Classes_Objetos/Visibilidade_atributos_métodos/index.php
+++ PHP - POO/Criando_Classes_Objetos/Visibilidade_atributos_métodos/index.php
@@ -11,22 +11,39 @@
 
 
 <body>
-
+<pre>
 	<?php 
 
 	require_once "Caneta.php"; 
 
-	$c1 = new caneta;  
+	$c1 = new Caneta; 
 
-	$c1 ->modelo = "Bic cristal"; 
-	
-	var_dump($c1);
+	// modelo e cor são atributos publicos, não dará erro mexer. 
+	$c1 ->modelo = "Bic Cristal"; 
+	$c1 ->cor = "Azul";
+
+	// vai dar erro, pois o atributo ponta é é private 
+	// $c1->ponta = 0.5;
+
+	// vai dar erro, pois carga é protected. 
+	// $c1 ->carga = 99; 
+
+	//vai dar erro, pois tampada é protected 
+	//$c1 ->tampada = true; 
+
+	print_r($c1);
+
+	// rabiscar é publico. 
+	$c1 -> rabiscar();
+
+	// vai dar erro, tampar é private
+	// $c1 -> tampar();
+
+
+	// Se o método for publico e o atributo for protegido, ele vai realizar o método sem erro. 
 	
 	?>
-
-
-
+</pre>
 </body>
-
 
 </html>
